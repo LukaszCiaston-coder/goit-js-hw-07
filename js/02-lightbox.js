17363e07a45ea600cfbf419d3135f66d2d7b4d6f
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-04-23 20:16:17 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+function createGalleryItem(item) {
+    const galleryItem = document.createElement('li');
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__item')
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image')
+   
+
+  galleryLink.href = item.original;
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  galleryImage.addEventListener('click', e => {
+    e.preventDefault();
+
+      var gallery = new SimpleLightbox('.gallery a', {
+          captions:true, captionDelay:250
+      });
+      
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
+
+  });
+
+  return galleryItem;
+}
+
+galleryItems.forEach(item => {
+  const galleryItem = createGalleryItem(item);
+  gallery.appendChild(galleryItem);
+});
 console.log(galleryItems);
