701174ee647139218870ac004c50fbe1968a4a3a
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-04-23 20:41:17 +0200

    "use strict"

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,4 @@
+"use strict";
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
@@ -17,8 +18,8 @@ function createGalleryItem(item) {
   galleryLink.appendChild(galleryImage);
   galleryItem.appendChild(galleryLink);
 
-  galleryImage.addEventListener('click', e => {
-    e.preventDefault();
+  galleryImage.addEventListener('click', event => {
+    event.preventDefault();
 
      var lightbox = new SimpleLightbox('.gallery a', {
         captionsData: 'alt',
